85577204befecae6bd270dd94f4d22bf1fdfab21
sanjeevareddy91 <gsanjeevreddy91@gmail.com>
2017-11-27 13:02:56 +0530

    First Commit

--- locallibrary/settings.py
+++ locallibrary/settings.py
@@ -80,42 +80,20 @@ WSGI_APPLICATION = 'locallibrary.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.10/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-#         'NAME': 'library',
-#         'USER': 'postgres',
-#         'PASSWORD': 'password',
-#         'HOST': 'localhost',
-#         'PORT': '5432',
-#     }
-# }
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'library2',
-        'USER': 'sanjeeva',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'library',
+        'USER': 'root',
         'PASSWORD': 'password',
-        'HOST': 'localhost',
-        'PORT': '5432',
+        'HOST':'localhost',
+        'PORT':'3306',
+        'OPTIONS': {
+          'autocommit': True,
+        },
     }
 }
-
-
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.mysql',
-#         'NAME': 'library',
-#         'USER': 'root',
-#         'PASSWORD': 'password',
-#         'HOST':'localhost',
-#         'PORT':'3306',
-#         'OPTIONS': {
-#           'autocommit': True,
-#         },
-#     }
-# }
 
 
 # Password validation
